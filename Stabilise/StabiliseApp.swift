ed516474b7523716bd81c47b31f20504d347b747
Mariyam <mariyam.tanveer99@gmail.com>
2024-11-15 15:41:32 +0100

    Initial Commit

---
+++ Stabilise/StabiliseApp.swift
@@ -0,0 +1,17 @@
+//
+//  StabiliseApp.swift
+//  Stabilise
+//
+//  Created by Mariyam Tanveer on 15/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct StabiliseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StabiliseTests/StabiliseTests.swift
@@ -0,0 +1,17 @@
+//
+//  StabiliseTests.swift
+//  StabiliseTests
+//
+//  Created by Mariyam Tanveer on 15/11/2024.
+//
+
+import Testing
+@testable import Stabilise
+
+struct StabiliseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
